ab988db3212b439e07fe2bede1349267298054cf
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2022-10-22 22:39:05 -0300

    Implementando serviço da interface ISearchPost

---
+++ OuvICEx.API/OuvICEx.API.Domain/Services/SearchPostService.cs
@@ -0,0 +1,23 @@
+﻿using OuvICEx.API.Domain.Entities;
+using OuvICEx.API.Domain.Interfaces;
+
+namespace OuvICEx.API.Domain.Services
+{
+    public class SearchPostService : ISearchPostService
+    {
+        private readonly IRepository _repository;
+
+        public SearchPostService(IRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public List<PostInfo> GetPostsBasedOnSelectionFilter(PostSelectionFilter filter)
+        {
+            List<PostInfo> posts = _repository.GetPostsBasedOnSelectionFilter(filter);
+
+            posts.RemoveAll(post => post.IsVisible == false);
+            return posts;
+        }
+    }
+}
