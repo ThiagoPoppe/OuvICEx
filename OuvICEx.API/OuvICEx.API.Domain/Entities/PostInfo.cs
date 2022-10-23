71c58f248ff6f9038bcf5454beca30c15166f06b
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2022-10-22 22:38:10 -0300

    Criando interfaces (portas) e entidades do domínio

---
+++ OuvICEx.API/OuvICEx.API.Domain/Entities/PostInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace OuvICEx.API.Domain.Entities
+{
+    public class PostInfo
+    {
+        public string? Content { get; set; }
+        public string? Context { get; set; }
+        public string? AuthorDepartament { get; set; }
+        public string? TargetDepartament { get; set; }
+
+        public bool IsVisible { get; set; }
+        public bool IsResolved { get; set; }
+
+        public string? CreatedDate { get; set; }
+    }
+}
---
+++ OuvICEx.API/OuvICEx.API.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using OuvICEx.API.Domain.Entities;
+
+namespace OuvICEx.API.Domain.Interfaces
+{
+    public interface IRepository
+    {
+        public List<PostInfo> GetPostsBasedOnSelectionFilter(PostSelectionFilter filter);
+    }
+}
