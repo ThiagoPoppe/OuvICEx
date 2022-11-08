e08cf610043b9431a30ee50a6ebca06ed90f5f10
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2022-11-08 19:55:18 -0300

    Implementando Mapper para conversão de entidades e modelos

---
+++ OuvICEx.API/OuvICEx.API.Domain/Profiles/PublicationProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using OuvICEx.API.Domain.Entities;
+using OuvICEx.API.Domain.Models;
+
+namespace OuvICEx.API.Domain.Profiles
+{
+    public class PublicationProfile : Profile
+    {
+        private bool HasTargetDepartament(Publication publication)
+            => publication.TargetDepartament != null;
+
+        private bool HasUserAndDepartament(Publication publication)
+            => publication.User != null && publication.User.Departament != null;
+
+        public PublicationProfile()
+        {
+            CreateMap<Publication, PublicationModel>()
+                .ForMember(f => f.AuthorDepartamentName, dest => dest.MapFrom(src => HasUserAndDepartament(src) ? src.User.Departament.Name : null))
+                .ForMember(f => f.TargetDepartamentName, dest => dest.MapFrom(src => HasTargetDepartament(src) ? src.TargetDepartament.Name : null));
+
+            CreateMap<PublicationCreationModel, Publication>()
+                .ForMember(f => f.Id, dest => dest.Ignore())
+                .ForMember(f => f.User, dest => dest.Ignore())
+                .ForMember(f => f.Status, dest => dest.Ignore())
+                .ForMember(f => f.TargetDepartament, dest => dest.Ignore());
+        }
+    }
+}
--- OuvICEx.API/OuvICEx.API/Controllers/PublicationController.cs
+++ OuvICEx.API/OuvICEx.API/Controllers/PublicationController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using OuvICEx.API.Domain.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
 using OuvICEx.API.Domain.Interfaces.Service;
 using OuvICEx.API.Domain.Models;
 
